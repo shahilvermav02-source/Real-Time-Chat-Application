5e014fa0ccb6debcad177bb90eecfe6cbc1c736a
Shahil Verma <shahil.vermav02@gmail.com>
2026-06-22 16:17:23 +0545

    Schema build

---
+++ src/models/message.models.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    room: {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
